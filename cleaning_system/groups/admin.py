2793fe41332e9dc98978ad3362ec9289dcd573b3
tox-in <habimanat78@gmail.com>
2024-11-18 09:45:12 +0200

    changes in group model functionality

--- cleaning_system/groups/admin.py
+++ cleaning_system/groups/admin.py
@@ -5,11 +5,3 @@ from .models import Group
 class GroupAdmin(admin.ModelAdmin):
     list_display = ['name', 'specialization', 'chief', 'rating']
     filter_horizontal = ['members']
-
-    def save_model(self, request, obj, form, change):
-        # Save the group instance first to generate an ID
-        obj.save()
-        # After saving, handle the members relation
-        if obj.members.exists():
-            obj.members.set(obj.members.all())  # Reassign members to ensure proper linkage
-        super().save_model(request, obj, form, change)
